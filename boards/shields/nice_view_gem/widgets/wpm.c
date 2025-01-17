1659246ec2b15b53b8566d51b644ac8ae1244d8e
hf02 <55464333+hf02@users.noreply.github.com>
2025-01-17 18:16:13 -0500

    remove needle

--- boards/shields/nice_view_gem/widgets/wpm.c
+++ boards/shields/nice_view_gem/widgets/wpm.c
@@ -123,8 +123,8 @@ static void draw_label(lv_obj_t *canvas, const struct status_state *state) {
 }
 
 void draw_wpm_status(lv_obj_t *canvas, const struct status_state *state) {
-    draw_gauge(canvas, state);
-    draw_needle(canvas, state);
+    //draw_gauge(canvas, state);
+    //draw_needle(canvas, state);
     draw_grid(canvas);
     draw_graph(canvas, state);
     draw_label(canvas, state);
