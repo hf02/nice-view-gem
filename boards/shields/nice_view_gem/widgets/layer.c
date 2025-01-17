32cd44bd270c96c90a61474f5a4a76f73a6297ab
hf02 <55464333+hf02@users.noreply.github.com>
2025-01-17 18:54:43 -0500

    more text styles

--- boards/shields/nice_view_gem/widgets/layer.c
+++ boards/shields/nice_view_gem/widgets/layer.c
@@ -4,9 +4,6 @@
 #include <lvgl.h>
 
 void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
-    lv_draw_label_dsc_t label_dsc;
-    init_label_dsc(&label_dsc, LVGL_BACKGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_CENTER);
-
     char text[10] = {};
 
     if (state->layer_label == NULL) {
@@ -16,12 +13,49 @@ void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
         to_uppercase(text);
     }
 
-    if (state->layer_index != 0) {
-        lv_draw_rect_dsc_t rect_dsc;
-        init_rect_dsc(&rect_dsc, LVGL_FOREGROUND);
-
-        lv_canvas_draw_rect(canvas, 0, 146 + BUFFER_OFFSET_BOTTOM, 68, BUFFER_OFFSET_BOTTOM + 146, &rect_dsc);
+    
+    switch (state->layer_index) {
+        case 0:
+            // dont render anything
+            break;
         
-        lv_canvas_draw_text(canvas, 0, 146 + BUFFER_OFFSET_BOTTOM, 68, &label_dsc, text);
+        // gaming layers
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+            // white text with a line above it
+            
+            // draw the top line...
+            lv_draw_rect_dsc_t rect_dsc_top;
+            init_rect_dsc(&rect_dsc_top, LVGL_FOREGROUND);
+            lv_canvas_draw_rect(canvas, 0, 146 + BUFFER_OFFSET_BOTTOM, 68, 1, &rect_dsc_top);
+
+            // ...and the bottom line
+            lv_draw_rect_dsc_t rect_dsc_bottom;
+            init_rect_dsc(&rect_dsc_bottom, LVGL_FOREGROUND);
+            lv_canvas_draw_rect(canvas, 0, 161 + BUFFER_OFFSET_BOTTOM, 68, 1, &rect_dsc_bottom);
+
+            // draw the text
+            lv_draw_label_dsc_t label_dsc;
+            init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_CENTER);
+            lv_canvas_draw_text(canvas, 0, 146 + BUFFER_OFFSET_BOTTOM, 68, &label_dsc, text);
+            break;
+
+        // everything else
+        default:
+            // black text on white background            
+
+            // draw the background
+            lv_draw_rect_dsc_t rect_dsc;
+            init_rect_dsc(&rect_dsc, LVGL_FOREGROUND);
+            lv_canvas_draw_rect(canvas, 0, 146 + BUFFER_OFFSET_BOTTOM, 68, 15, &rect_dsc);
+
+            // draw the text
+            lv_draw_label_dsc_t label_dsc;
+            init_label_dsc(&label_dsc, LVGL_BACKGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_CENTER);
+            lv_canvas_draw_text(canvas, 0, 146 + BUFFER_OFFSET_BOTTOM, 68, &label_dsc, text);
+            break;
     }
+
 }
