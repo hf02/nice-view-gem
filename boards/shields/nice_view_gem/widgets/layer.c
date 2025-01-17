a9455d8e4ad1fd9090f345b8a140110c36956325
hf02 <55464333+hf02@users.noreply.github.com>
2025-01-17 17:10:56 -0500

    attempt to make the layer label more pronunced

--- boards/shields/nice_view_gem/widgets/layer.c
+++ boards/shields/nice_view_gem/widgets/layer.c
@@ -1,10 +1,11 @@
 #include <zephyr/kernel.h>
 #include "layer.h"
 #include "../assets/custom_fonts.h"
+#include <lvgl.h>
 
 void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_label_dsc_t label_dsc;
-    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_CENTER);
+    init_label_dsc(&label_dsc, LVGL_BACKGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_CENTER);
 
     char text[10] = {};
 
@@ -16,6 +17,11 @@ void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
     }
 
     if (state->layer_index != 0) {
+        lv_draw_rect_dsc_t rect_dsc;
+        init_rect_dsc(&rect_dsc, LVGL_FOREGROUND);
+
+        lv_canvas_draw_rect(canvas, 0, 146 + BUFFER_OFFSET_BOTTOM, 68, BUFFER_OFFSET_BOTTOM + 146, &rect_dsc);
+        
         lv_canvas_draw_text(canvas, 0, 146 + BUFFER_OFFSET_BOTTOM, 68, &label_dsc, text);
     }
 }
