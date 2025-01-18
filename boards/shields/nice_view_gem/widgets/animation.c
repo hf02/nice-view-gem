84a0ed328088d07295c5abda0cd46e98ea4a3afe
hf02 <55464333+hf02@users.noreply.github.com>
2025-01-17 22:02:07 -0500

    reenable image

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -5,7 +5,6 @@
 LV_IMG_DECLARE(crystal_01);
 
 void draw_animation(lv_obj_t *canvas) {
-    return;
     lv_obj_t *art = lv_img_create(canvas);
     lv_img_set_src(art, &crystal_01);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
